182beb4a3373a8d200cf7fbd281d3c737eb7f90b
NET-C2150 <timothyhughest1811@gmail.com>
2022-02-06 15:15:51 -0700

    Simple window to wrap asset browser widget

---
+++ AssetBrowser/AssetBrowserWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Tools;
+
+public class AssetBrowserWindow : Window
+{
+
+	public AssetBrowserWindow( Widget parent = null, Action<Asset> onAssetPicked = null )
+		: base( parent )
+	{
+		Size = new Vector2( 756, 500 );
+		Title = "Asset Browser";
+		Canvas = new Widget( this );
+		var l = Canvas.MakeTopToBottom();
+		var browser = l.Add( new AssetBrowser( this ) );
+		browser.OnAssetPicked += ( asset ) =>
+		{
+			onAssetPicked?.Invoke( asset );
+			Close();
+		};
+
+		Show();
+	}
+
+}
